c358a4d66c0c475a0dd7bbccd4c95aac05322729
Rafa Ramírez <raffe90@gmail.com>
2015-10-17 16:39:43 +0200

    Adds Sponsored Posts

--- blog.rb
+++ blog.rb
@@ -8,10 +8,19 @@ class Blog
 		@posts.push(post)
 	end
 
+	def print_post(post)
+			if post.sponsored
+				puts "******#{post.title}******"
+			else
+				puts "#{post.title}"
+			end
+				print "**************\n#{post.text}\n----------------\n#{post.date}\n----------------\n"
+	end
+
 	def publish_front_page
 		sorted_posts = @posts.sort {|a,b| b.date <=> a.date}
 		sorted_posts.each do |post|
-			puts "#{post.title}\n**************\n#{post.text}\n----------------\n#{post.date}\n----------------\n"
+			print_post(post)
 		end
 	end
 end
--- post.rb
+++ post.rb
@@ -1,11 +1,12 @@
 require 'date'
 
 class Post
-	attr_reader :title, :date, :text
+	attr_reader :title, :date, :text, :sponsored
 
-	def initialize(title, date, text)
+	def initialize(title, date, text, sponsored = false)
 		@title = title
 		@date = Date.jd(date.to_i)
 		@text = text
+		@sponsored = sponsored
 	end
 end
