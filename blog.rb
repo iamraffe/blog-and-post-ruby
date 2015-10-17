9ec5b3b011ac19c246770ec4fb5b673fa6b9ce4f
Rafa Ramírez <raffe90@gmail.com>
2015-10-17 17:01:31 +0200

    Adds basic pagination

--- blog.rb
+++ blog.rb
@@ -1,7 +1,10 @@
 require "pry"
 class Blog
+	attr_accessor :current_page
+
 	def initialize(posts = [])
 		@posts = posts
+		@current_page = 1
 	end
 
 	def add_post(post)
@@ -17,9 +20,15 @@ class Blog
 				print "**************\n#{post.text}\n----------------\n#{post.date}\n----------------\n"
 	end
 
+	def select_posts(posts)
+		paginate = @current_page * 3
+		[posts[paginate-3],posts[paginate-2],posts[paginate-1]]
+	end
+
 	def publish_front_page
 		sorted_posts = @posts.sort {|a,b| b.date <=> a.date}
-		sorted_posts.each do |post|
+		selected_posts = select_posts(sorted_posts)
+		selected_posts.each do |post|
 			print_post(post)
 		end
 	end
--- wordpress.rb
+++ wordpress.rb
@@ -5,5 +5,20 @@ blog = Blog.new
 blog.add_post Post.new('Post Title 1', '2451944', 'Post Text 1')
 blog.add_post Post.new('Post Title 2', '2651944', 'Post Text 2', true)
 blog.add_post Post.new('Post Title 3', '2351944', 'Post Text 3')
+blog.add_post Post.new('Post Title 4', '2453334', 'Post Text 1')
+blog.add_post Post.new('Post Title 6', '2651444', 'Post Text 2', true)
+blog.add_post Post.new('Post Title 5', '2351194', 'Post Text 3')
+blog.add_post Post.new('Post Title 7', '2451914', 'Post Text 1')
+blog.add_post Post.new('Post Title 8', '2651224', 'Post Text 2', true)
+blog.add_post Post.new('Post Title 9', '2351344', 'Post Text 3')
 
 blog.publish_front_page
+while true
+	puts """
+#{blog.current_page} #{blog.current_page+1} #{blog.current_page+2}
+
+> next
+"""
+	blog.current_page = gets.chomp.to_i
+	blog.publish_front_page
+end
